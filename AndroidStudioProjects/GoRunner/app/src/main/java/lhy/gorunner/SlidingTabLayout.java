d385c97235f680e73e6e1ae2510105e9518f06f4
xiaohao94 <HAUYANG@LIMs-MacBook-Pro.local>
2016-01-24 15:51:22 +0800

    Changed the selector color to increase the visibility of user experience

--- AndroidStudioProjects/GoRunner/app/src/main/java/lhy/gorunner/SlidingTabLayout.java
+++ AndroidStudioProjects/GoRunner/app/src/main/java/lhy/gorunner/SlidingTabLayout.java
@@ -223,7 +223,10 @@ public class SlidingTabLayout extends HorizontalScrollView {
             if (i == mViewPager.getCurrentItem()) {
                 tabView.setSelected(true);
             }
+            tabTitleView.setTextColor(getResources().getColorStateList(R.color.selector));
+            tabTitleView.setTextSize(14);
         }
+
     }
 
     public void setContentDescription(int i, String desc) {
