955875f42b9e37e6408f9ba19ad988fafbce672a
xiaohao94 <HAUYANG@LIMs-MacBook-Pro.local>
2016-02-19 01:02:13 +0800

    Added new activity <Task Details> and added a number of components into the layout.

--- AndroidStudioProjects/GoRunner/app/src/main/java/lhy/gorunner/Browse_Activity.java
+++ AndroidStudioProjects/GoRunner/app/src/main/java/lhy/gorunner/Browse_Activity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class Browse_Activity extends AppCompatActivity {
 
@@ -27,4 +30,27 @@ public class Browse_Activity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
